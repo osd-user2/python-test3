dbab36ae41ca84b82df054ee66e86aec54a149eb
osd-user2 <34326639+osd-user2@users.noreply.github.com>
2018-03-29 17:09:56 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Network test to Pod1"
+    return "Network test to Pod3"
 
 if __name__ == "__main__":
     application.run()
